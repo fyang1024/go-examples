df7fa8240cc552edc4ad5e50c96a8f2e356000ef
fyang1024 <fyang1024@gmail.com>
2019-03-07 11:43:56 +1100

    interfaces

---
+++ src/github.com/fyang1024/examples/interfaces.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+/**
+ * In Golang, interface are a special type which includes only func declarations without func key word.
+ * interfaces are implicitly implemented. You just need to implement all methods defined in interface.
+ */
+
+type geometry interface {
+	area() float64
+	perim() float64
+}
+
+type rect struct {
+	width, height float64
+}
+
+type circle struct {
+	radius float64
+}
+
+func (r rect) area() float64 {
+	return r.width * r.height
+}
+
+func (r rect) perim() float64 {
+	return 2*r.width + 2*r.height
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (c circle) perim() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+func measure(g geometry) {
+	fmt.Println(g)
+	fmt.Println(g.area())
+	fmt.Println(g.perim())
+}
+
+func main() {
+	r := rect{width: 3, height: 4}
+	c := circle{radius: 5}
+
+	measure(r)
+	measure(c)
+}
